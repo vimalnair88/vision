5530ba6ef8515177bccdefe2d7e2dd9d1ab54d01
Vimal Nair <Vimal_Nair@intuit.com>
2017-10-12 10:54:17 -0700

    Vimal Nair: Final Changes

--- src/main/java/main/ImageCapture.java
+++ src/main/java/main/ImageCapture.java
@@ -5,18 +5,30 @@ import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.WebcamResolution;
 
 public class ImageCapture {
 
 	public void imageCapture(){
+		Dimension[] nonStandardResolutions = new Dimension[] {
+				WebcamResolution.PAL.getSize(),
+				WebcamResolution.WQHD.getSize(),
+				new Dimension(2000, 1000),
+				new Dimension(1000, 500),
+			};
 		Webcam webcam = Webcam.getDefault();
-		webcam.setViewSize(new Dimension(640, 480));
+		webcam.setCustomViewSizes(nonStandardResolutions);
+		webcam.setViewSize(WebcamResolution.WQHD.getSize());
 		webcam.open();
 		try {
 			ImageIO.write(webcam.getImage(), "JPG", new File("testImage.jpg"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	public static void main(String args[]){
+		ImageCapture imageCapture = new ImageCapture();
+		imageCapture.imageCapture();
 	}
 	
 }
